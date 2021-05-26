6c3704c3d3eb21a2eebc029608dfcc21f9bcc389
Jonathas <jonathas-25@hotmail.com>
2021-05-26 12:11:40 -0300

    #3 continuação
    Código exemplo da aula.

--- DOO/aula1.java
+++ DOO/aula1.java
@@ -1,3 +1,17 @@
-public class aula1 {
-    
+public class Pessoa {
+    int idade;
+    string sexo;
+    string nome;
+
+    public static void main(String[] args) {
+        Pessoa qualquer = new Pessoa();
+        qualquer.nome = "Jonathas";
+        qualquer.idade = 25;
+        qualquer.sexo = "M";
+        system.out.printld("O nome é" + qualquer.nome);
+        system.out.printld("A idade é " + qualquer.idade);
+        system.out.printld("O sexo é" + qualquer.sexo);
+
+    }
+
 }
